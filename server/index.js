d0ac67c030117bd50e3dc8e0a76b86d4d83263da
merchant151 <jmiller32@ggc.edu>
2021-10-04 14:26:43 -0400

    printing user twish data to console

--- server/index.js
+++ server/index.js
@@ -9,6 +9,14 @@ const router = express.Router();
 const app = express();
 const app2 = express();
 
+//configureing express to use bodyParser
+app2.use(bodyParser.urlencoded({ extended: true }));
+app2.use(bodyParser.json());
+console.log("bodyParser set");
+
+//app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.json());
+
 //attempting to enable cors polocy
 app2.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -21,15 +29,18 @@ app.get('/', function(req, res, next) {
   // Handle the get for this route
 });
 */
-app2.post('/', function(req, res, next) {
+//bodyParser,
+app2.post('/',  function(req, res, next) {
  // Handle the post for this route
  console.log("POST triggered at / instead of /handle");
 //failled line
  //console.log("body of post: "+ JSON.stringify(req.body, null, 2));
 //attempt two
-console.log("req of POST: "+ req +" " + JSON.stringify(req.body) +" "+req);
-console.log("res of POST: "+ res+" "+ res.body);
-console.log("next of POST: "+ next+" "+ res.body);
+//bodyParser.urlencoded({extended: true});
+console.log("req of POST: "+ req +" req.body stringify" + JSON.stringify(req.body) +
+  "req.params ");
+//console.log("res of POST: "+ res+" "+ JSON.stringify(req));
+console.log(`statusCode: ${res.statusCode}`);
  res.end('Passed to client.')
 });
 
@@ -37,10 +48,7 @@ app.get("/api", (req, res) => {
   res.json({ message: "Attempting post!" });
 });
 
-//configureing express to use bodyParser
-app2.use(bodyParser.urlencoded({ extended: false }));
-app2.use(bodyParser.json());
-console.log("times i am triggered");
+
 /* I am going to try app post next.
 router.post("/handle",(reqPost,resPost) => {
 //code to perform particular action.
